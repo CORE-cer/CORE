5b46fbbb8fd43813fec5e834acbdf9e49519a9d5
Kyle <kylevon1@gmail.com>
2023-06-01 13:37:23 -0400

    Improved interface of compile_and_test script

--- scripts/compile_and_test.sh
+++ scripts/compile_and_test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+PURPLE='\033[0;35m'
 NORMAL_OUTPUT='\033[0m'
 
 # Change to parent directory
@@ -9,5 +10,12 @@ cd "$(dirname "$0")/.."
 # Compile the project under "Debug"
 ./scripts/build.sh
 
+echo -e "${PURPLE}--------------${NORMAL_OUTPUT}"
+echo -e "${PURPLE}Internal Tests${NORMAL_OUTPUT}"
+echo -e "${PURPLE}--------------${NORMAL_OUTPUT}"
 ./build/Debug/tests
+
+echo -e "${PURPLE}-------------------------${NORMAL_OUTPUT}"
+echo -e "${PURPLE}Third Party Library Tests${NORMAL_OUTPUT}"
+echo -e "${PURPLE}-------------------------${NORMAL_OUTPUT}"
 ./build/Debug/third_party_tests
