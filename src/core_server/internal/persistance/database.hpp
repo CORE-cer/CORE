3c85415c5912bfa8f271a52635cbde7f125c78c6
Kyle <kylevon1@gmail.com>
2023-06-08 13:25:16 -0400

    Changed format to satisfly clang-format and also changed the connection attribute to be a std::unique_ptr.

--- src/core_server/internal/persistance/database.hpp
+++ src/core_server/internal/persistance/database.hpp
@@ -1,79 +1,90 @@
-#include <iostream> // Include the iostream header for std::cout and std::cerr
-#include <vector>
-#include <string>
-#include "shared/datatypes/catalog/attribute_info.hpp"
+#include <iostream>  // Include the iostream header for std::cout and std::cerr
 #include <pqxx/pqxx>
+#include <string>
+#include <vector>
+
+#include "shared/datatypes/catalog/attribute_info.hpp"
+
+using namespace CORETypes;
 
 namespace DatabaseManager {
-    class Value;
+class Value;
 
-    class Database {
-    private:
-        pqxx::connection connection;
+class Database {
+ private:
+  std::unique_ptr<pqxx::connection> connection;
 
-    public:
-        Database() {
-            // Connect to the database
-            std::cout << "Probando" << std::endl;
+ public:
+  Database() {
+    // Connect to the database
+    std::cout << "Probando" << std::endl;
 
-            connect_database();
-        }
+    connect_database();
+  }
 
-        void add_event_type(std::string&& event_name, std::vector<AttributeInfo>&& event_attributes, uint64_t event_type_id);
+  void add_event_type(std::string&& event_name,
+                      std::vector<AttributeInfo>&& event_attributes,
+                      uint64_t event_type_id);
 
-        void add_stream_type(std::string stream_name, std::vector<uint64_t>&& stream_event_types, uint64_t stream_type_id);
-            
-        void connect_database() {
-            try {
-                pqxx::connection conn("dbname=core user=postgres password=postgres host=localhost port=5432");
-                if (conn.is_open()) {
-                    std::cout << "Connected to the database." << std::endl;
-                    connection = std::move(conn);
+  void add_stream_type(std::string stream_name,
+                       std::vector<uint64_t>&& stream_event_types,
+                       uint64_t stream_type_id);
 
-                    // Call create_table() to create the necessary tables
-                    create_table();
+  void connect_database() {
+    try {
+      std::unique_ptr<pqxx::connection> conn =
+          std::make_unique<pqxx::connection>(
+              "dbname=core user=postgres password=postgres host=localhost "
+              "port=5432");
+      if (conn->is_open()) {
+        std::cout << "Connected to the database." << std::endl;
+        connection = std::move(conn);
 
-                    // Call fill_table() to insert data into tables if needed
-                    fill_table();
-                }
-            } catch (const std::exception& e) {
-                std::cerr << "Error connecting to the database: " << e.what() << std::endl;
-            }
-        }
+        // Call create_table() to create the necessary tables
+        create_table();
 
-    private:
-        void create_table() {
-            try {
-                pqxx::work txn(connection);
-                // Create the necessary SQL statements to create tables
-                std::string eventTableSQL = "CREATE TABLE IF NOT EXISTS events("
-                                            "event_id SERIAL PRIMARY KEY,"
-                                            "event_name VARCHAR(255),"
-                                            "event_attributes JSONB"
-                                            ");";
+        // Call fill_table() to insert data into tables if needed
+        fill_table();
+      }
+    } catch (const std::exception& e) {
+      std::cerr << "Error connecting to the database: " << e.what()
+                << std::endl;
+    }
+  }
 
-                std::string streamTableSQL = "CREATE TABLE IF NOT EXISTS streams ("
-                                            "stream_id SERIAL PRIMARY KEY,"
-                                            "stream_name VARCHAR(255),"
-                                            "stream_event_types JSONB"
-                                            ");";
+ private:
+  void create_table() {
+    try {
+      pqxx::work txn(*connection);
+      // Create the necessary SQL statements to create tables
+      std::string eventTableSQL =
+          "CREATE TABLE IF NOT EXISTS events("
+          "event_id SERIAL PRIMARY KEY,"
+          "event_name VARCHAR(255),"
+          "event_attributes JSONB"
+          ");";
 
-                // Execute the SQL statements to create tables
-                txn.exec(eventTableSQL);
-                txn.exec(streamTableSQL);
+      std::string streamTableSQL =
+          "CREATE TABLE IF NOT EXISTS streams ("
+          "stream_id SERIAL PRIMARY KEY,"
+          "stream_name VARCHAR(255),"
+          "stream_event_types JSONB"
+          ");";
 
-                txn.commit();
-                std::cout << "Tables created successfully." << std::endl;
-            } catch (const std::exception& e) {
-                std::cerr << "Error creating tables: " << e.what() << std::endl;
-            }
-        }
+      // Execute the SQL statements to create tables
+      txn.exec(eventTableSQL);
+      txn.exec(streamTableSQL);
 
-        void fill_table() {
-            // Implementation to insert data into tables if needed
-        }
+      txn.commit();
+      std::cout << "Tables created successfully." << std::endl;
+    } catch (const std::exception& e) {
+      std::cerr << "Error creating tables: " << e.what() << std::endl;
+    }
+  }
 
+  void fill_table() {
+    // Implementation to insert data into tables if needed
+  }
+};
 
-    };
-
-}
+}  // namespace DatabaseManager
