6e58d39e145887f68cfd49bf2cdaa670d25a951a
Kyle <kylevon1@gmail.com>
2023-06-22 11:38:32 -0400

    is_Star is now false by default.

--- src/core_server/internal/ceql/query/select.hpp
+++ src/core_server/internal/ceql/query/select.hpp
@@ -20,9 +20,9 @@ struct Select {
   bool is_star;
 
   Select(Strategy&& strategy, std::set<std::string>&& variable_names,
-         bool&& is_star)
+         bool is_star)
       : strategy(std::move(strategy)),
         variable_names(std::move(variable_names)),
-        is_star(std::move(is_star)) {}
+        is_star(is_star) {}
 };
 }  // namespace InternalCORECEQL
