e6b1b09fcfb2c08b38f67dcd3a89f5f7575eabb5
Kyle <kylevon1@gmail.com>
2023-06-28 15:54:26 -0400

    Created clang_format_all_files script and used it on the project

--- src/core_server/internal/parsing/ceql_query/autogenerated/CEQLQueryParserBaseVisitor.cpp
+++ src/core_server/internal/parsing/ceql_query/autogenerated/CEQLQueryParserBaseVisitor.cpp
@@ -1,7 +1,4 @@
 
 // Generated from CEQLQueryParser.g4 by ANTLR 4.12.0
 
-
 #include "CEQLQueryParserBaseVisitor.h"
-
-
--- src/core_server/internal/parsing/stream_declaration/autogenerated/StreamDeclarationParserVisitor.cpp
+++ src/core_server/internal/parsing/stream_declaration/autogenerated/StreamDeclarationParserVisitor.cpp
@@ -1,7 +1,4 @@
 
 // Generated from StreamDeclarationParser.g4 by ANTLR 4.12.0
 
-
 #include "StreamDeclarationParserVisitor.h"
-
-
