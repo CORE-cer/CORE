bf737c03c9c5bfdd8283f64f9842e39843b437e2
juanelsch <juan.lpez@uc.cl>
2023-09-26 18:36:42 -0300

    refactor: non-sequencing and sequencing names

--- src/core_server/internal/evaluation/logical_cea/transformations/constructions/contiguous_sequencing.hpp
+++ src/core_server/internal/evaluation/logical_cea/transformations/constructions/contiguous_sequencing.hpp
@@ -8,27 +8,21 @@
 
 namespace CORE::Internal::CEA {
 
-class Sequencing : public LogicalCEATransformer<Sequencing> {
+class ContiguousSequencing
+    : public LogicalCEATransformer<ContiguousSequencing> {
  public:
   using VariablesToMark = mpz_class;
   using EndNodeId = uint64_t;
 
-  // The difference between sequencing and concat is that
-  // In sequencing there is a wildcard loop on the destination states.
+  // Note, for this to work the implementation of union must offset the
+  // right_states by the amount of states in the left_cea, and not make
+  // another arbitrary permutation.
   LogicalCEA eval(LogicalCEA& left, LogicalCEA& right) {
     LogicalCEA out = Union()(left, right);
     out.initial_states = left.initial_states;
     out.final_states = right.final_states << left.amount_of_states;
     auto left_final_states = left.get_final_states();
     auto right_initial_states = right.get_initial_states();
-
-    // Tautology loop
-    for (auto right_initial_state : right_initial_states) {
-      uint64_t target_state_id = right_initial_state + left.amount_of_states;
-      out.transitions[target_state_id].emplace_back(
-        PredicateSet(PredicateSet::Type::Tautology), 0, target_state_id);
-    }
-
     for (auto left_final_state : left_final_states) {
       for (auto right_initial_state : right_initial_states) {
         EndNodeId target = right_initial_state + left.amount_of_states;
