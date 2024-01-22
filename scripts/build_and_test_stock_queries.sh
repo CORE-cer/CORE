b128a3eb69773cae45133bfd69be6c38d100b16c
Nicolas Buzeta <nicolas@buzeta.net>
2024-01-22 01:02:55 -0300

    feat(stock-queries): add all tests for stock queries

--- scripts/build_and_test_stock_queries.sh
+++ scripts/build_and_test_stock_queries.sh
@@ -20,6 +20,12 @@ queries=$(find "$base_dir/queries" -type f)
 # FIX by having the port numbers be dynamic if already used
 # parallel --verbose --halt now,fail=1 "$run_and_compare_script $executable '{} $base_dir/stock_data.csv' $base_dir/expected_results/{/}" ::: $queries
 
+echo -e "Checking if expected_results folder exists"
+if ! test -f $base_dir/expected_results; then
+    echo -e "expected_results folder not found, uncompressing"
+    tar -xf $base_dir/expected_results.tar.xz --directory $base_dir
+fi
+
 for query in $queries; do
     echo -e "Running ${query}"
     query_file=$(basename "$query")
@@ -29,6 +35,7 @@ for query in $queries; do
     $run_and_compare_script $executable "$query $base_dir/stock_data.csv" "$base_dir/expected_results/$query_file"
     if [ $? -ne 0 ]; then
         echo "One or more queries did not match the expected results."
+        echo "Check if expected_results folder is up-to-date with tar.xz"
         exit 1
     fi
 done
