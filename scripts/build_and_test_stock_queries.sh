bd0f2bd5fd1cf8132d8572f6e4e530482ef2a2cb
Nicolas Buzeta <nicolas@buzeta.net>
2024-01-22 00:37:39 -0300

    feat(scripts): add code for generating expected results

--- scripts/build_and_test_stock_queries.sh
+++ scripts/build_and_test_stock_queries.sh
@@ -23,6 +23,9 @@ queries=$(find "$base_dir/queries" -type f)
 for query in $queries; do
     echo -e "Running ${query}"
     query_file=$(basename "$query")
+    # if ! test -f "$base_dir/expected_results/$query_file"; then
+    #     ./build/Debug/offline_experiment_stocks /home/nicobuzeta/Documents/CORE-Project/CORE/src/targets/experiments/stocks/queries/$query_file /home/nicobuzeta/Documents/CORE-Project/CORE/src/targets/experiments/stocks/stock_data.csv | tee /home/nicobuzeta/Documents/CORE-Project/CORE/src/targets/experiments/stocks/expected_results/$query_file
+    # fi
     $run_and_compare_script $executable "$query $base_dir/stock_data.csv" "$base_dir/expected_results/$query_file"
     if [ $? -ne 0 ]; then
         echo "One or more queries did not match the expected results."
