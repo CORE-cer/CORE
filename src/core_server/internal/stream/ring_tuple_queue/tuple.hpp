6f66f427627dabf43863bf9f22c3640d8c3d5ed8
Nicolas Buzeta <nicolas@buzeta.net>
2024-01-29 11:58:31 -0300

    feat(tuple-queue): use atomic for memory visibility between threads

--- src/core_server/internal/stream/ring_tuple_queue/tuple.hpp
+++ src/core_server/internal/stream/ring_tuple_queue/tuple.hpp
@@ -1,6 +1,7 @@
 #ifndef TUPLE_HPP
 #define TUPLE_HPP
 
+#include <atomic>
 #include <cassert>
 #include <chrono>
 #include <cstdint>
@@ -165,6 +166,8 @@ class Tuple {
   std::string serialize_data() const {
     // Use memcpy or some other way so that the data ptr is sent as a string, so that
     // it is then converted directly to a uint64_t* pos.
+    std::atomic<uint64_t*> atomic_data;
+    atomic_data.store(data, std::memory_order_release);
     std::string out(sizeof(uint64_t*), '\0');
     std::memcpy(&out[0], &data, sizeof(uint64_t*));
     return out;
