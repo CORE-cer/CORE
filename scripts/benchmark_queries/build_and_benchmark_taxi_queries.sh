697da29dfd909c1dc82f42bca734a42743179577
Nicolas Buzeta <nicolas@buzeta.net>
2024-03-07 16:52:50 -0300

    feat(benchmark): update benchmark code to use declaration

--- scripts/benchmark_queries/build_and_benchmark_taxi_queries.sh
+++ scripts/benchmark_queries/build_and_benchmark_taxi_queries.sh
@@ -15,6 +15,7 @@ base_dir="src/targets/experiments/taxis"
 executable="build/${BUILD_TYPE}/offline_experiment_taxis"
 csv="taxi_data.csv"
 compressed_csv="taxi_data.tar.xz"
+declaration="declaration.core"
 benchmark_file="$base_dir/benchmark.csv"
 
 queries=$(find "$base_dir/queries" -type f | sort -V) 
@@ -32,7 +33,7 @@ echo "query,time" >$benchmark_file
 for query in $queries; do
     echo -e "Running ${query}"
     query_file=$(basename "$query")
-    time_taken=$(/usr/bin/time -f "%e" bash -c 'for i in {1..3}; do '"$executable"' '"$base_dir"'/queries/'"$query_file"' '"$base_dir"'/'"$csv"' > '"/dev/null"'; done' 2>&1)
+    time_taken=$(/usr/bin/time -f "%e" bash -c 'for i in {1..3}; do '"$executable"' '"$base_dir"'/queries/'"$query_file"' '"$base_dir"'/'"$declaration"' '"$base_dir"'/'"$csv"' > '"/dev/null"'; done' 2>&1)
     avg_time=$(echo "$time_taken / $repeats" | bc -l)
     echo "$query_file,$avg_time" >> "$benchmark_file"
 done
