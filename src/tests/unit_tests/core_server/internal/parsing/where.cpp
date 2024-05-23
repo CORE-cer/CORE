8a6441e7ed99b5876d81712e5a2b5be8eece32c4
Benjamín Vicente <benjavicente@uc.cl>
2024-05-23 19:51:42 -0400

    test(unless): parse test for unless

--- src/tests/unit_tests/core_server/internal/parsing/where.cpp
+++ src/tests/unit_tests/core_server/internal/parsing/where.cpp
@@ -112,6 +112,16 @@ TEST_CASE("event or event", "[Where]") {
   REQUIRE(formula->equals(expected_formula.get()));
 }
 
+TEST_CASE("event unless event", "[UNLESS]") {
+  auto query = create_where_query("H UNLESS T");
+  auto expected_formula = make_unique<UnlessFormula>(make_unique<EventTypeFormula>("H"),
+                                                     make_unique<EventTypeFormula>("T"));
+  auto formula = parse_formula(query);
+  INFO("Expected: " + expected_formula->to_string());
+  INFO("Got: + formula -> " + formula->to_string());
+  REQUIRE(formula->equals(expected_formula.get()));
+}
+
 // Filters checked at filter.cpp
 
 TEST_CASE("Example test NonContiguous", "[Where, Composite] NonContiguous") {
