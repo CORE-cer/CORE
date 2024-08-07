6a79fb3cd1674c0278a039dcf9e9c35169bfe36b
Nicolas Buzeta <nicolas@buzeta.net>
2024-08-07 12:04:52 -0400

    update flake

--- flake.nix
+++ flake.nix
@@ -3,8 +3,10 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
@@ -14,7 +16,10 @@
             bashInteractive
             zsh
             clang-tools
+            parallel
+            conan
           ];
         };
-      });
+      }
+    );
 }
