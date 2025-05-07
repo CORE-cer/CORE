bc71b03eb4ad74d4f834140ddf428ea07ba2d6a9
Nicolas Buzeta <nicolas@buzeta.net>
2025-05-07 19:06:53 -0400

    cmore

--- src/core_server/internal/ceql/query/select.hpp
+++ src/core_server/internal/ceql/query/select.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <cassert>
+#include <map>
 #include <memory>
+#include <set>
 #include <string>
 #include <utility>
 
@@ -9,14 +11,32 @@
 namespace CORE::Internal::CEQL {
 
 struct Select {
+  using VariableName = std::string;
+  using StreamName = std::string;
+  using AttributeName = std::string;
+
   enum class Strategy { ALL, ANY, LAST, MAX, NEXT, STRICT, DEFAULT = Strategy::ALL };
 
   std::unique_ptr<Formula> formula;
   Strategy strategy;
   bool is_star;
 
-  Select(Strategy&& strategy, bool is_star, std::unique_ptr<Formula>&& formula)
-      : strategy(std::move(strategy)), is_star(is_star), formula(std::move(formula)) {}
+  // Attribute projection
+  std::map<VariableName, std::set<AttributeName>> attribute_projection_variable;
+  std::map<std::pair<StreamName, VariableName>, std::set<AttributeName>>
+    attribute_projection_stream_event;
+
+  Select(Strategy&& strategy,
+         bool is_star,
+         std::unique_ptr<Formula>&& formula,
+         std::map<VariableName, std::set<AttributeName>>&& attribute_projection_variable,
+         std::map<std::pair<StreamName, VariableName>, std::set<AttributeName>>&&
+           attribute_projection_stream_event)
+      : strategy(std::move(strategy)),
+        is_star(is_star),
+        formula(std::move(formula)),
+        attribute_projection_variable(std::move(attribute_projection_variable)),
+        attribute_projection_stream_event(std::move(attribute_projection_stream_event)) {}
 
   std::string to_string() const {
     std::string out = "Select " + strategy_to_string(strategy);
@@ -30,7 +50,13 @@ struct Select {
     return out + "}";
   }
 
-  Select clone() const { return Select(Strategy(strategy), is_star, formula->clone()); }
+  Select clone() const {
+    return Select(Strategy(strategy),
+                  is_star,
+                  formula->clone(),
+                  std::map(attribute_projection_variable),
+                  std::map(attribute_projection_stream_event));
+  }
 
  private:
   static std::string strategy_to_string(Strategy strat) {
