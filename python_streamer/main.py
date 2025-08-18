c87dce1e86f27f260e8cd8d5b9efd8f6f1c76f97
Nicolas Buzeta <nicolas@buzeta.net>
2025-08-18 01:19:43 -0400

    update python version to docker version

--- python_streamer/main.py
+++ python_streamer/main.py
@@ -3,7 +3,6 @@ import asyncio
 import _pycore
 from bluesky.create_post import CreatePostStreamer
 from coinbase.ticker import TickerStreamer
-from coinbase.ticker_offline import TickerStreamerOffline
 
 
 async def do():
@@ -11,13 +10,13 @@ async def do():
 
     streamer = _pycore.PyStreamer("tcp://localhost", 5002)
 
-    # ticker_streamer = TickerStreamer(client, streamer)
+    ticker_streamer = TickerStreamer(client, streamer)
     # ticker_streamer = TickerStreamerOffline(client, streamer)
 
     bluesky_post_streamer = CreatePostStreamer(client, streamer)
 
     tasks = []
-    # tasks.append(ticker_streamer.start())
+    tasks.append(ticker_streamer.start())
     tasks.append(bluesky_post_streamer.start())
     await asyncio.gather(*tasks)
 
