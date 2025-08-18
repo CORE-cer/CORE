c764fab7a2796a582f593fc3ec80aab187d1932f
Nicolas Buzeta <nicolas@buzeta.net>
2025-08-17 22:22:03 -0400

    working bluesky

--- python_streamer/bluesky/models/commit.py
+++ python_streamer/bluesky/models/commit.py
@@ -1,20 +1,65 @@
+import datetime
 from typing import List, Literal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+# class PostRecordModel(BaseModel):
+#     langs: List[str]
+#     text: str
+#
+#
+# class CreatePostCommitModel(BaseModel):
+#     operation: Literal["create"]
+#     collection: Literal["app.bsky.feed.post"]
+#     record: PostRecordModel
+#
+#
+# class EventCommitCreatePostModel(BaseModel):
+#     kind: Literal["commit"]
+#     time_us: int
+#     commit: CreatePostCommitModel
+
+
+class RecordSubjectModel(BaseModel):
+    cid: str
+    uri: str
 
 
 class PostRecordModel(BaseModel):
+    record_type: Literal["app.bsky.feed.post"] = Field(alias="$type")
+    createdAt: datetime.datetime
     langs: List[str]
     text: str
 
 
-class CreatePostCommitModel(BaseModel):
+class FollowRecordModel(BaseModel):
+    record_type: Literal["app.bsky.graph.follow"] = Field(alias="$type")
+    createdAt: datetime.datetime
+    subject: str
+
+
+class LikeRecordModel(BaseModel):
+    record_type: Literal["app.bsky.feed.like"] = Field(alias="$type")
+    createdAt: datetime.datetime
+    subject: RecordSubjectModel
+
+
+class RepostRecordModel(BaseModel):
+    record_type: Literal["app.bsky.feed.repost"] = Field(alias="$type")
+    createdAt: datetime.datetime
+    subject: RecordSubjectModel
+
+
+class CommitEventModel(BaseModel):
+    cid: str
     operation: Literal["create"]
-    collection: Literal["app.bsky.feed.post"]
-    record: PostRecordModel
+    record: (
+        PostRecordModel | FollowRecordModel | LikeRecordModel | RepostRecordModel
+    ) = Field(discriminator="record_type")
 
 
-class EventCommitCreatePostModel(BaseModel):
+class CommitWrapperEventModel(BaseModel):
+    did: str
     kind: Literal["commit"]
     time_us: int
-    commit: CreatePostCommitModel
+    commit: CommitEventModel
--- src/shared/datatypes/value.hpp
+++ src/shared/datatypes/value.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <cstdint>
+#include <cstdio>
 #include <ctime>
 #include <limits>
 #include <memory>
 #include <string>
+#include <string_view>
 
 namespace CORE::Types {
 struct Value {
@@ -16,6 +18,116 @@ struct Value {
 };
 
 struct StringValue final : public Value {
+  static std::size_t extra_space(const std::string_view s) noexcept {
+    std::size_t result = 0;
+
+    for (const auto& c : s) {
+      switch (c) {
+        case '"':
+        case '\\':
+        case '\b':
+        case '\f':
+        case '\n':
+        case '\r':
+        case '\t': {
+          // from c (1 byte) to \x (2 bytes)
+          result += 1;
+          break;
+        }
+
+        default: {
+          if (c >= 0x00 and c <= 0x1f) {
+            // from c (1 byte) to \uxxxx (6 bytes)
+            result += 5;
+          }
+          break;
+        }
+      }
+    }
+
+    return result;
+  }
+
+  static std::string escape_string(const std::string_view s) noexcept {
+    const auto space = extra_space(s);
+    if (space == 0) {
+      return std::string(s);
+    }
+
+    // create a result string of necessary size
+    std::string result(s.size() + space, '\\');
+    std::size_t pos = 0;
+
+    for (const auto& c : s) {
+      switch (c) {
+        // quotation mark (0x22)
+        case '"': {
+          result[pos + 1] = '"';
+          pos += 2;
+          break;
+        }
+
+        // reverse solidus (0x5c)
+        case '\\': {
+          // nothing to change
+          pos += 2;
+          break;
+        }
+
+        // backspace (0x08)
+        case '\b': {
+          result[pos + 1] = 'b';
+          pos += 2;
+          break;
+        }
+
+        // formfeed (0x0c)
+        case '\f': {
+          result[pos + 1] = 'f';
+          pos += 2;
+          break;
+        }
+
+        // newline (0x0a)
+        case '\n': {
+          result[pos + 1] = 'n';
+          pos += 2;
+          break;
+        }
+
+        // carriage return (0x0d)
+        case '\r': {
+          result[pos + 1] = 'r';
+          pos += 2;
+          break;
+        }
+
+        // horizontal tab (0x09)
+        case '\t': {
+          result[pos + 1] = 't';
+          pos += 2;
+          break;
+        }
+
+        default: {
+          if (c >= 0x00 and c <= 0x1f) {
+            // print character c as \uxxxx
+            sprintf(&result[pos + 1], "u%04x", int(c));
+            pos += 6;
+            // overwrite trailing null character
+            result[pos] = '\\';
+          } else {
+            // all other characters are added as-is
+            result[pos++] = c;
+          }
+          break;
+        }
+      }
+    }
+
+    return result;
+  }
+
   std::string val{""};
 
   StringValue() {}
@@ -30,7 +142,7 @@ struct StringValue final : public Value {
 
   std::string to_string() const override { return val; }
 
-  std::string to_json() const override { return "\"" + to_string() + "\""; }
+  std::string to_json() const override { return "\"" + escape_string(to_string()) + "\""; }
 
   std::string get_type() const override { return "StringValue"; }
 
