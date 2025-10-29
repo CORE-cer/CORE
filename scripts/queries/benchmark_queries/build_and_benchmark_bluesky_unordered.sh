ef0ad773140b97ea47daa1b06a19f87dd6e04892
Nicolas Buzeta <nicolas@buzeta.net>
2025-10-29 16:35:25 -0300

    add benchmarks

---
+++ scripts/queries/benchmark_queries/build_and_benchmark_bluesky_unordered.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Work at the root directory
+# Should have conanfile.py present there.
+cd "$(dirname "$0")"
+cd ../../..
+
+source scripts/common.sh
+_setArgs "$@"
+
+# Call build function from common
+build
+
+base_dir="src/targets/experiments/unordered_bluesky"
+executable="build/${BUILD_TYPE}/offline"
+csv="bluesky_unordered.corecsv"
+declaration="declaration.core"
+quarantine_declaration="quarantine_declaration.core"
+benchmark_file="$base_dir/benchmark.csv"
+
+queries=$(find "$base_dir/queries" -type f | sort -V) 
+TIMEFORMAT=%R
+
+repeats=3
+
+echo "query,time" >$benchmark_file
+for query in $queries; do
+    echo -e "Running ${query}"
+    query_file=$(basename "$query")
+    time_taken=$(/usr/bin/time -f "%e" bash -c 'for i in {1..3}; do '"$executable"' '"-q $base_dir"'/queries/'"$query_file"' '"-d $base_dir"'/'"$declaration"' '"-c $base_dir"'/'"$csv"' > '"/dev/null"'; done' 2>&1)
+    avg_time=$(echo "$time_taken / $repeats" | bc -l)
+    echo "$query_file,$avg_time" >> "$benchmark_file"
+done
