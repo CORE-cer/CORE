b1f6f54fc25dad02164d87f3160720bcc3e75587
Nicolas Buzeta <nicolas@buzeta.net>
2025-10-29 22:29:09 -0300

    update query scripts

--- scripts/queries/base_queries/build_and_test_taxi_queries.sh
+++ scripts/queries/base_queries/build_and_test_taxi_queries.sh
@@ -11,17 +11,13 @@ _setArgs "$@"
 # Call build function from common
 build
 
-run_and_compare_script="scripts/run_and_compare.sh"
 base_dir="src/targets/experiments/taxis"
 executable="build/${BUILD_TYPE}/offline"
 csv="taxi_data.csv"
 declaration="declaration.core"
 compressed_csv="taxi_data.tar.xz"
 
-queries=$(find "$base_dir/queries" -type f) 
-
-# FIX by having the port numbers be dynamic if already used
-# parallel --verbose --halt now,fail=1 "$run_and_compare_script $executable '{} $base_dir/stock_data.csv' $base_dir/expected_results/{/}" ::: $queries
+queries=$(find "$base_dir/queries" -type f | sort -V)
 
 echo -e "Checking if expected_results folder exists"
 if ! test -d $base_dir/expected_results; then
@@ -38,10 +34,7 @@ fi
 for query in $queries; do
     echo -e "Running ${query}"
     query_file=$(basename "$query")
-    # if ! test -f "$base_dir/expected_results/$query_file"; then
-    #     { time $executable $base_dir/queries/$query_file $base_dir/$csv > $base_dir/expected_results/$query_file ; } 2> $base_dir/expected_results/"${query_file}_time.txt"
-    # fi
-    $run_and_compare_script $executable "-q $query -d $base_dir/$declaration -c $base_dir/$csv" "$base_dir/expected_results/$query_file"
+    scripts/run_single_query_and_check.sh "$executable" "$query" "$base_dir/$declaration" "$base_dir/$csv" "$base_dir/expected_results/$query_file"
     if [ $? -ne 0 ]; then
         rm -rf $base_dir/expected_results
         rm -f $base_dir/$csv
@@ -50,7 +43,6 @@ for query in $queries; do
         exit 1
     fi
 done
-# Check if parallel succeeded
 
 rm -rf $base_dir/expected_results
 rm -f $base_dir/$csv
--- scripts/queries/benchmark_queries/build_and_benchmark_stock_queries.sh
+++ scripts/queries/benchmark_queries/build_and_benchmark_stock_queries.sh
@@ -1,32 +1,12 @@
 #!/bin/bash
 
 # Work at the root directory
-# Should have conanfile.py present there.
 cd "$(dirname "$0")"
 cd ../../..
 
-source scripts/common.sh
-_setArgs "$@"
+export BENCHMARK_BASE_DIR="src/targets/experiments/stocks"
+export BENCHMARK_CSV="stock_data.csv"
+export BENCHMARK_DECLARATION="declaration.core"
+export BENCHMARK_COMPRESSED_RESULTS="expected_results.tar.xz"
 
-# Call build function from common
-build
-
-base_dir="src/targets/experiments/stocks"
-executable="build/${BUILD_TYPE}/offline"
-csv="stock_data.csv"
-declaration="declaration.core"
-benchmark_file="$base_dir/benchmark.csv"
-
-queries=$(find "$base_dir/queries" -type f | sort -V) 
-TIMEFORMAT=%R
-
-repeats=3
-
-echo "query,time" >$benchmark_file
-for query in $queries; do
-    echo -e "Running ${query}"
-    query_file=$(basename "$query")
-    time_taken=$(/usr/bin/time -f "%e" bash -c 'for i in {1..3}; do '"$executable"' '"-q $base_dir"'/queries/'"$query_file"' '"-d $base_dir"'/'"$declaration"' '"-c $base_dir"'/'"$csv"' &> '"/dev/null"'; done' 2>&1)
-    avg_time=$(echo "$time_taken / $repeats" | bc -l)
-    echo "$query_file,$avg_time" >> "$benchmark_file"
-done
+exec scripts/queries/benchmark_queries/benchmark_runner.sh "$@"
