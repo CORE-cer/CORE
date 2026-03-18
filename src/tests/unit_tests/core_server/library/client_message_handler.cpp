e722ea36fdd06b9b31d654e50f1de1ca350f8efe
Nicolas Buzeta <nicolas@buzeta.net>
2026-03-18 08:32:58 -0300

    fix(clang-tidy)

--- src/tests/unit_tests/core_server/library/client_message_handler.cpp
+++ src/tests/unit_tests/core_server/library/client_message_handler.cpp
@@ -52,10 +52,10 @@ TEST_CASE("ClientMessageHandler returns an error response for unknown request ty
   auto result_handler_factory = std::make_shared<OfflineResultHandlerFactory>();
   ClientMessageHandler handler(backend, backend_mutex, result_handler_factory);
 
-  const auto invalid_request = Types::ClientRequest("",
-                                                    static_cast<Types::ClientRequestType>(
-                                                      255));
-  const auto serialized_request = Internal::ClientRequestCodec::serialize(invalid_request);
+  const auto valid_request = Types::ClientRequest("",
+                                                  Types::ClientRequestType::AddQuery);
+  auto serialized_request = Internal::ClientRequestCodec::serialize(valid_request);
+  serialized_request.back() = static_cast<char>(0xff);
 
   const auto serialized_response = handler(serialized_request);
   const auto response = Internal::CerealSerializer<Types::ServerResponse>::deserialize(
