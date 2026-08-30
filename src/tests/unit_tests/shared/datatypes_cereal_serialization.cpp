fe4aa90468b02da84bdfc82d8adce46d1f1246a6
Nicolas Buzeta <nicolas@buzeta.net>
2026-08-30 19:44:42 -0400

    test: use unit test namespace

--- src/tests/unit_tests/shared/datatypes_cereal_serialization.cpp
+++ src/tests/unit_tests/shared/datatypes_cereal_serialization.cpp
@@ -30,6 +30,10 @@ TEST_CASE("Serialization works for all basic CORE types", "[cereal, core_types]"
   check_serialization_of<Types::DateValue, std::time_t>(200);
 }
 
+}  // namespace CORE::Internal::ThirdPartyTests
+
+namespace CORE::Internal::Shared::UnitTests::DatatypesCerealSerialization {
+
 TEST_CASE("Event to_string includes variable name when present", "[core_types]") {
   Types::Event event(0, std::vector<std::unique_ptr<Types::Value>>{});
   event.variable_name = "a";
@@ -37,4 +41,4 @@ TEST_CASE("Event to_string includes variable name when present", "[core_types]")
   REQUIRE(event.to_string() == "(variable_name: a id: 0 attributes: [])");
 }
 
-}  // namespace CORE::Internal::ThirdPartyTests
+}  // namespace CORE::Internal::Shared::UnitTests::DatatypesCerealSerialization
